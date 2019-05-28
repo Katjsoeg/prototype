9fbde918d8789366dbdbc3f80378fc9ff54fbd6f
katj2950 <KoDe071185>
2019-05-28 09:15:22 +0200

    accordion

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-function collapsiple() {
-	var coll = document.getElementsByClassName("collapsible");
-	var i;
+let acc = document.getElementsByClassName("accordion");
+let i;
 
-	for (i = 0; i < coll.length; i++) {
-		coll[i].addEventListener("click", function () {
-			this.classList.toggle("active");
-			var content = this.nextElementSibling;
-			if (content.style.display === "block") {
-				content.style.display = "none";
-			} else {
-				content.style.display = "block";
-			}
-		});
-	}
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function () {
+		/* Toggle between adding and removing the "active" class,
+		to highlight the button that controls the panel */
+		this.classList.toggle("active");
+
+		/* Toggle between hiding and showing the active panel */
+		let panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+		} else {
+			panel.style.display = "block";
+		}
+	});
 }
