116e561fa53438aa2a89f8393aef41e5951f3b08
katj2950 <KoDe071185>
2019-05-28 13:09:58 +0200

    burgermenu attempt

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ for (i = 0; i < acc.length; i++) {
 		}
 	});
 }
+
+//Burgermenu
+
+
+function myFunction() {
+  let x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
