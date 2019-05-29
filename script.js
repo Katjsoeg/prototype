b593133d1947f3e71b73f7614d9293d9000211b1
katj2950 <KoDe071185>
2019-05-29 10:16:36 +0200

    burgermenu styling

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ for (i = 0; i < acc.length; i++) {
 }
 
 //Burgermenu
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+
+}
+
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+    }
+}
